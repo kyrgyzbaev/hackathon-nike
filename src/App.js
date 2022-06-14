4418b4819b0614578de0121561073d3011eaa1e7
kyrgyzbaev <baygazykyrgyzbaev97@gmail.com>
2022-06-14 13:30:16 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/fire.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/app";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMBhYymrhIqfVz_6L_ccWsQV0iGEIMB3Y",
+  authDomain: "react-hackaton.firebaseapp.com",
+  projectId: "react-hackaton",
+  storageBucket: "react-hackaton.appspot.com",
+  messagingSenderId: "412491220677",
+  appId: "1:412491220677:web:c8de5b9f4ae908f1f412ac",
+};
+
+export default firebase.initializeApp(firebaseConfig);
